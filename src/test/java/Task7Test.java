0de3ebd862f977a14a257f5145b4198625061d97
Elmira Mandzhieva <enmandzhieva@gmail.com>
2024-09-13 22:35:04 +0300

    done task 7

---
+++ src/test/java/Task7Test.java
@@ -0,0 +1,50 @@
+import org.assertj.core.api.Assertions;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+
+public class Task7Test {
+    private static WebDriver webDriver;
+    private static WebDriverWait wait;
+
+    @BeforeClass
+    public static void start() {
+        webDriver = new ChromeDriver();
+        wait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
+    }
+
+    @Test
+    public void clickAllItemsInMenuAndCheckHeaders() {
+        webDriver.get("http://localhost/litecart/");
+
+        By allProductsXpath = By.xpath("//li[@class='product column shadow hover-light']");
+        By stickersXpath = By.xpath(".//div[contains(@class,'sticker')]");
+
+        List<WebElement> products = webDriver.findElements(allProductsXpath);
+        Assertions.assertThat(products).as("Не найдены элементы продуктов").isNotEmpty();
+        SoftAssertions assertions = new SoftAssertions();
+
+        for (int i = 0; i < products.size(); i++) {
+            assertions.assertThat(
+                    products.get(i).findElements(stickersXpath)).as(String.format("У продукта [%d] количество стикеров не равно 1", i)).hasSize(1);
+        }
+        assertions.assertAll();
+    }
+
+
+    @After
+    public void stop() {
+        webDriver.quit();
+        webDriver = null;
+    }
+}
